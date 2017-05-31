2c38f325c579d9b6d5a9f92640beb4ec86cedd9e
Gonzalo Solera <gonzalo1796@gmail.com>
2017-05-31 20:45:11 +0200

    Fixing and finishing squares

--- Lab6/squares.pl
+++ Lab6/squares.pl
@@ -10,7 +10,7 @@ ejemplo(5, 175, [81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,
 ejemplo(6, 100, [1,2,3,4,5,6,7]).
 
 main :- 
-    ejemplo(0, Big, Sides),
+    ejemplo(1, Big, Sides),
     nl, write('Fitting all squares of size '), write(Sides), write(' into big square of size '), write(Big), nl, nl,
     length(Sides, N), 
     length(RowVars, N), % get list of N prolog vars: Row coordinates of each small square
@@ -19,35 +19,43 @@ main :-
     Vars ins 1..Big,
     insideBigSquare(Big, Sides, RowVars),
     insideBigSquare(Big, Sides, ColVars),
-    nonOverLapping(Sides, RowVars, ColVars),
+    nonOverLapping(N, Sides, RowVars, ColVars),
     label(Vars),
     displaySol(N, Sides, RowVars, ColVars), halt.
+
+zip(0, _, _, _, []).
+zip(K, [S|Sides], [R|RowVars], [C|ColVars], [K-S-R-C|L]) :-
+    M is K-1,
+    zip(M, Sides, RowVars, ColVars, L).
 
 insideBigSquare(_, [], []) :- !.
 insideBigSquare(Big, [S|Sides], [C|CoordVars]) :-
     C #=< Big - S + 1,
     insideBigSquare(Big, Sides, CoordVars).
 
-nonOverLapping(Sides, RowVars, ColVars) :- 
-    findall(K-S-X-Y, (nth1(K, Sides, S), nth1(K, RowVars, X), nth1(K, ColVars, Y)), Squares),
-    findall((S1-X1-Y1, S2-X2-Y2), (member(K1-S1-X1-Y1, Squares), member(K2-S2-X2-Y2, Squares), K1 \= K2), Pairs),
-    nonOverLapping(Pairs).
+nonOverLapping(N, Sides, RowVars, ColVars) :-
+    zip(N, Sides, RowVars, ColVars, Squares),
+    nonOverLapping(Squares, Squares).
 
-nonOverLapping([]) :- !.
-nonOverLapping([(S1-X1-Y1, S2-X2-Y2)| Pairs]) :-
-    trace,
-    write(S1-S2), nl,
+nonOverLapping([], _) :- !.
+nonOverLapping([K-S-X-Y|Others], Squares) :- 
+    nonOverLappingForSquare(K-S-X-Y, Squares),
+    nonOverLapping(Others, Squares).
+
+nonOverLappingForSquare(_, []) :- !.
+nonOverLappingForSquare(K-S-X-Y, [K-_-_-_|Squares]) :- 
+    !, nonOverLappingForSquare(K-S-X-Y, Squares).
+nonOverLappingForSquare(K-S1-X1-Y1, [_-S2-X2-Y2|Squares]) :-
     exprOverlapping(S1-X1, S2-X2, Expr1),
-    exprOverlapping(S1-Y1, S2-Y2, Expr1),
-    #\ ((Expr1) #/\ (Expr2)),
-    nonOverLapping(Pairs).
+    exprOverlapping(S1-Y1, S2-Y2, Expr2),
+    (#\ ((Expr1) #/\ (Expr2))),
+    nonOverLappingForSquare(K-S1-X1-Y1, Squares).
 
 exprOverlapping(S1-C1, S2-C2, (Expr1 #\/ Expr2)) :-
-    exprBetween(C1, C2, C1+S1, Expr1),
-    exprBetween(C1, C2+S2, C1+S1, Expr2).
+    exprBetween(C1, C2, C1+S1-1, Expr1),
+    exprBetween(C1, C2+S2-1, C1+S1-1, Expr2).
 
 exprBetween(A, B, C, (A #=< B) #/\ (B #=< C)).
-
 
 displaySol(N, Sides, RowVars, ColVars) :- 
     between(1, N, Row), nl, between(1, N, Col),
